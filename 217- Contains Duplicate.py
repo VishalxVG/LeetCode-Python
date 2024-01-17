f5f4e56d43ce76533b38a720b2cd630858cb790c
Vishal Ghosh <avatarvishal1551@gmail.com>
2024-01-17 20:52:46 +0530

    217 LeetCode

--- 217- Contains Duplicate.py
+++ 217- Contains Duplicate.py
@@ -1,8 +1,12 @@
-# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ #? >> Given an integer array nums, return true if any value appears at least twice in the array, 
+ #?    and return false if every element is distinct.
 
-# Brute Force Approach 
+
 
 class Solution:
+    
+    #* Brute Force Approach ->> O(n^2) 
+
     def containsDuplicate(nums) -> bool:
         for i in range(len(nums)-1):
             for j in range(i+1 , len(nums)):
@@ -10,6 +14,23 @@ class Solution:
                     return True
                 
         return False
+    
+    #* Sorting (we sort then we check adjacent elements) ->> O(n logn)
+    def containsDuplicateSort(nums) -> bool:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1] :
+                return True
+        return False    
+    
+
+    
+
 if __name__ == "__main__":
     nums = [1,2,3,4]
-    print(Solution.containsDuplicate(nums))
+    
+    print(Solution.containsDuplicate(nums)) #OUTPUT - FALSE
+    print(Solution.containsDuplicateSort(nums))
+
+
+
